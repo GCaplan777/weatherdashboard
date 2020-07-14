5605ddd12d40af9e03e77a29fe72779871395f64
GCaplan <gabriel.caplan@gmail.com>
2020-07-14 16:46:25 -0400

    pseudo coding in JS

---
+++ Assets/script.js
@@ -0,0 +1,29 @@
+// We have a website featuring weather
+// user types in search form for city
+//  click "search" button
+// search function is run through API
+// site displays weather conditions from API
+// Main section = current weather conditions
+// SubSection = future conditions
+// Left column is populated with search history
+
+// Main section display:
+// Row1: city name, date, icon representing weather considetion
+// temparture
+// humidity
+// wind speed
+// UV index: featuring: color that indicates conditions favorable, moderate, or severe
+
+// Subsection displays 5 columns each with
+// date
+// 5-day forecast
+// an icon representation of weather conditions,
+// the temperature
+// the humidity
+
+// City name will be stored in left-hand column with it's DATA.
+// WHEN user clicks on a city in the search history
+// User is again presented with DATA for that city (could be updated if later in day and user is still in the same browser?)
+
+// If the user closes and opens the weather dashboard,
+// user sees last searched city forecast
